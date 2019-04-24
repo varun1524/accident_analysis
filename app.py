6b759b54a2320a5fc912120d79c6d25ee2293acb
Varun Shah <varun.shah2612@gmail.com>
2019-04-23 19:27:09 -0700

    flask server intialize

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import traceback
+
+from service.accident_severity_service import AccidentSeverityService
 
 app = Flask(__name__)
-
+accident_severity_service = AccidentSeverityService()
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/predict', methods=["POST"])
+def predict_severity():
+    status = 404
+    res_data = {}
+    msg = {"msg": "Error while predicting severity"}
+    try:
+        input_data = request.json
+        status, res_data, msg = accident_severity_service.predict_accident_severity(input_data)
+        return render_template("index.html", status=status, res_data=res_data, msg=msg)
+    except Exception:
+        print(traceback.format_exc())
+        return render_template("index.html", status=status, res_data=res_data, msg=msg)
 
 
 if __name__ == '__main__':
---
+++ service/accident_severity_service.py
@@ -0,0 +1,14 @@
+class AccidentSeverityService:
+    def __init__(self):
+        # predict input
+        pass
+
+    def validate_input(self):
+        pass
+
+    def preprocessing_input(self):
+        pass
+
+    def predict_accident_severity(self, input_data):
+        pass
+
